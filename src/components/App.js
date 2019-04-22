6327dcd8548fec24b7edad30a028933d4ade71b6
unknown <kevin.cheung.kai@gmail.com>
2019-04-22 15:24:40 -0400

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Lottery from './Lottery';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Lottery />
+      </div>
+    );
+  }
+}
+
+export default App;
